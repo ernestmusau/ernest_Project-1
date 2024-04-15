7945d5baefc91bfc99ac5a4ff23d1dfc2412bae8
ernestmusau <ernest.musau@student.moringaschool.com>
2024-04-15 09:52:02 +0300

    completed

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const properties = [
       { id: 1, title: 'Luxury Apartment', price: '$500,000', image: 'Assets/apartment.jpg' },
       { id: 2, title: 'Cozy Home', price: '$300,000', image: 'Assets/cozy.jpeg' },
-      { id: 3, title: 'Modern Villa', price: '$1,200,000', image: 'Assets/villa.jpg' }
+      { id: 3, title: 'Modern Villa', price: '$1,200,000', image: 'Assets/villa.jpg' },
+      { id: 4, "title": "Bungalow", "price": "$240,000", "image": "Assets/bungalow.jpg" },
+      { id: 5, "title": "Studio", "price": "$100,000", "image": "Assets/studio.jpg" },
+      { id: 6, "title": "Maisonette", "price": "$1,350,000", "image": "Assets/maisonette.jpeg" }
     ];
   
     // Display properties on the page
@@ -32,4 +35,21 @@ document.addEventListener('DOMContentLoaded', function() {
       
     }
   });
+
+  function bookProperty(btn) {
+    const bookDiv = document.querySelector("#property-num");
+    const properties = bookDiv.textContent.split(" ")[0];
+    if (properties > 0) {
+        bookDiv.textContent = properties - 1 + " remaining properties";
+    }
+    else  {
+        let bookBtn = document.getElementById("book-now")
+        alert("No more properties!");//output is given to the user that the tickets are sold out
+        bookDivDiv.textContent = 'Sold Out';
+      bookBtn.textContent = 'Sold Out';
+        bookBtn.disabled = true;
+        a.target.classList.add("already-sold");
+        a.target.classList.remove("Blue");
+    }}
   
+ 
